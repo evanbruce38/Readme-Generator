bc8e3c72b4b4bc49fa08eb244783dc16966a1c02
evanbruce38 <106777653+evanbruce38@users.noreply.github.com>
2022-08-11 16:45:29 -0400

    added license link function

--- Utils/generateMarkdown.js
+++ Utils/generateMarkdown.js
@@ -22,7 +22,25 @@ function renderLicenseBadge(license) {
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license === "GNU AGPLv3") {
+    return "[GNU AGPLv3 License](https://www.gnu.org/licenses/agpl-3.0)";
+  } else if (license === "GNU GPLv3") {
+    return "[GNU GPLv3 License](https://www.gnu.org/licenses/gpl-3.0)";
+  } else if (license === "GNU LGPLv3") {
+    return "[GNU LGPLv3 License](https://www.gnu.org/licenses/lgpl-3.0)";
+  } else if (license === "Mozilla Public License 2.0") {
+    return "[Mozilla Public License 2.0](https://opensource.org/licenses/MPL-2.0)";
+  } else if (license === "Apache License 2.0") {
+    return "[Apache License 2.0](https://opensource.org/licenses/Apache-2.0)";
+  } else if (license === "MIT License") {
+    return "[MIT License](https://opensource.org/licenses/MIT)";
+  } else if (license === "Boost Software License 1.0") {
+    return "[Boost Software License 1.0](https://www.boost.org/LICENSE_1_0.txt)";
+  } else if (license === "The Unlicense") {
+    return "[The Unlicense](http://unlicense.org/)";
+  };
+};
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
