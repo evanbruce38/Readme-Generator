9ebcd22caec2cab1cad09345b8bfafc380e7c82b
evanbruce38 <106777653+evanbruce38@users.noreply.github.com>
2022-08-11 15:57:51 -0400

    added array of questions

--- index.js
+++ index.js
@@ -1,7 +1,59 @@
 // TODO: Include packages needed for this application
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'projectTitle',
+        message: 'What is the title of your project?'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'What is a description of your project?'
+    },
+    {
+        type: 'input',
+        name: 'tableOfContents',
+        message: 'Please list the table of contents for your project.'
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'What are the steps required to install your project?'
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Please provide instructions for how to use your app, provide screenshots as needed.'
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Which license would you like to apply to your app?'
+        choices: ['GNU AGPLv3', 'GNU GPLv3', 'GNU LGPLv3', 'Movilla Public License 2.0', 'Apache License 2.0', 'MIT License', 'Boost Software License 1.0', 'The Unlicense']
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'If you would like to have other developers contribute to your project, please specify the guidelines you would like them to follow.'
+    },
+    {
+        type: 'input',
+        name: 'credits',
+        message: 'please list your collaborators, if any, with links to their github profiles.'
+    },
+    {
+        type: 'input',
+        name: 'test',
+        message: 'Please describe how users may be able to run tests for your application.'
+    },
+    {
+        type: 'input',
+        name: 'questions',
+        message: 'Please provide an email for users to reach out to if they have further questions about your application.'
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
