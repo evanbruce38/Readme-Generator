dca539be4894870276b124383b5faef5a27fecdd
evanbruce38 <106777653+evanbruce38@users.noreply.github.com>
2022-08-11 17:19:43 -0400

    testing application

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ const questions = [
     {
         type: 'list',
         name: 'license',
-        message: 'Which license would you like to apply to your app?'
+        message: 'Which license would you like to apply to your app?',
         choices: ['GNU AGPLv3', 'GNU GPLv3', 'GNU LGPLv3', 'Mozilla Public License 2.0', 'Apache License 2.0', 'MIT License', 'Boost Software License 1.0', 'The Unlicense']
     },
     {
@@ -63,6 +63,7 @@ function writeReadmeFile(answers) {}
 
 // TODO: Create a function to initialize app
 function init() {
+  inquirer.prompt(questions)
     .then((answers) => {
         console.log(answers)
         const generatedReadMe = generateMarkdown(answers);
